c66c5d8d795309971d911613e17a440c2d85f948
TheWhaleHACK <85257185+TheWhaleHACK@users.noreply.github.com>
2026-02-02 15:16:43 +0300

    ов3_3

--- OpenNextWindow.cs
+++ OpenNextWindow.cs
@@ -6,106 +6,64 @@ using Unigine;
 public class OpenNextWindow : Component
 {
     private WorldTrigger trigger;
-    [ShowInEditor]
-    private ObjectMeshStatic thisScreen;
-    [ShowInEditor]
-    private ObjectMeshStatic nextScreen;
-
-    // Добавим переменную для отслеживания состояния
-    private bool isCurrentScreenActive = false;
+    [ShowInEditor] private ObjectMeshStatic thisScreen;
+    [ShowInEditor] private ObjectMeshStatic nextScreen;
 
     void Init()
     {
-        // Предположим, что при инициализации экран активен
-        isCurrentScreenActive = true;
-
+        // write here code to be called on component initialization
         trigger = node as WorldTrigger;
         if (trigger != null)
         {
-            trigger.EventEnter.Connect(OnChangeScreen);
+            trigger.EventEnter += OnTriggerEnter; // Используем += для подключения делегата
         }
         else
         {
-             Log.Warning("OpenNextWindow: Trigger is not a WorldTrigger or is null on node {0}\n", node.Name);
+            Log.Error("OpenNextWindow: Node is not a WorldTrigger! Node: {0}\n", node.Name);
         }
     }
 
-    private void OnChangeScreen()
+    // Метод, который будет вызван при срабатывании триггера
+    private void OnTriggerEnter(Node entered_node) // Имя параметра может зависеть от сигнатуры EventEnter в вашей версии Unigine
     {
-        // Проверяем, является ли этот компонент ответственным за активный экран
-        if (!isCurrentScreenActive)
+        // Проверяем, активен ли наш "thisScreen"
+        if (thisScreen != null && thisScreen.Enabled)
         {
-            // Если нет, игнорируем событие
-            return;
-        }
+            Log.Message("OpenNextWindow: Trigger '{0}' activated for screen '{1}'. Switching to '{2}'.\n",
+                        node.Name, thisScreen.Name, nextScreen != null ? nextScreen.Name : "null");
 
-        Log.Message("OpenNextWindow: Trigger '{0}' activated for screen '{1}'. Switching to '{2}'.\n",
-                    node.Name, thisScreen != null ? thisScreen.Name : "null", nextScreen != null ? nextScreen.Name : "null");
-
-        // Отключаем текущий экран и его триггер
-        if (thisScreen != null)
-        {
+            // Отключаем текущий экран и его детей
             thisScreen.Enabled = false;
-            // Отключаем детей текущего экрана
             for(int i = 0; i < thisScreen.NumChildren; i++)
             {
-                var child = thisScreen.GetChild(i);
-                // Убедитесь, что дочерний элемент не является самим триггером,
-                // если он не должен быть отключен как часть "внешнего" объекта экрана.
-                // Обычно триггеры находятся отдельно, но если он действительно дочерний - возможно, его нужно обрабатывать отдельно.
-                // Для простоты предположим, что дети - это UI/Visuals, а не сами триггеры.
-                // Если триггер находится внутри thisScreen, и вы хотите отключить его через Enabled родителя - этого может быть достаточно.
-                // Но если триггер отдельно, то его нужно отключать явно здесь:
-                if (child == trigger && trigger != null) continue; // Пропускаем сам триггер, если он дочерний и мы его отключаем ниже
-                child.Enabled = false;
+                thisScreen.GetChild(i).Enabled = false;
             }
-        }
 
-        // Отключаем сам триггер, чтобы он не реагировал больше
-        if (trigger != null)
-        {
-            trigger.Enabled = false; // Это отключает физический триггер
-        }
-
-        // Помечаем, что этот компонент больше не активен
-        isCurrentScreenActive = false;
-
-        // Включаем следующий экран
-        if (nextScreen != null)
-        {
-            nextScreen.Enabled = true;
-            for(int i = 0; i < nextScreen.NumChildren; i++)
+            // Включаем следующий экран и его детей
+            if (nextScreen != null)
             {
-                 nextScreen.GetChild(i).Enabled = true;
+                nextScreen.Enabled = true;
+                for(int i = 0; i < nextScreen.NumChildren; i++)
+                {
+                    nextScreen.GetChild(i).Enabled = true;
+                }
             }
-            // Логично было бы проверить, есть ли на следующем экране триггеры,
-            // которые должны стать активными, но это зависит от вашей архитектуры.
-            // Если следующий экран имеет свои компоненты OpenNextWindow, они будут ждать своего триггера.
+            else
+            {
+                Log.Warning("OpenNextWindow: nextScreen is not assigned for trigger '{0}' on screen '{1}'.\n", node.Name, thisScreen.Name);
+            }
+        }
+        else
+        {
+            // Логируем, если нужно отладить, что триггер сработал, но экран не был активен
+            // Log.Debug("OpenNextWindow: Trigger '{0}' fired, but its screen '{1}' was not active.\n", node.Name, thisScreen != null ? thisScreen.Name : "null");
+            // Не делаем ничего, если экран не активен
         }
     }
-
-    // Если нужно будет вернуться к этому экрану позже, можно будет добавить метод Reset или Enable
-    public void ActivateScreen()
-    {
-         if (thisScreen != null)
-         {
-             thisScreen.Enabled = true;
-             for(int i = 0; i < thisScreen.NumChildren; i++)
-             {
-                  thisScreen.GetChild(i).Enabled = true;
-             }
-             if (trigger != null)
-             {
-                 trigger.Enabled = true; // Включаем триггер снова
-             }
-             isCurrentScreenActive = true; // Теперь он снова активен
-         }
-    }
-
 
     void Update()
     {
         // foreach(var screen in thisScreen)
-        //     Log.MessageLine(screen.Name + " " + screen.Enabled);
+        // 	Log.MessageLine(screen.Name + " " + screen.Enabled);
     }
 }
